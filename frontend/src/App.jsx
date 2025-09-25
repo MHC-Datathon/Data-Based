8b2ba50371ff176bd12c5471a913b096bf99f321
addinar <raddina24@gmail.com>
2025-09-24 20:17:34 -0400

    added graph component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import {store} from './store';
-import KeplerMap from './components/KeplerMap';
-// src/App.jsx
+
 import KeplerEmbed from './components/KeplerEmbed';
 
 export default function App() {
